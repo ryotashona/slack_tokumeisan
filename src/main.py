b644b829255d87b59e203468ab05b7c713373cfd
ryosho2525 <ryosho.home@gmail.com>
2023-04-09 20:55:55 +0900

    tokumei_command #2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-import anon_post_handler as tokumei
+import tokumei
 
 app = App()
+
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+channel_id = os.environ["CHANNEL_ID"]
+channel_info = client.conversations_info(channel=channel_id)
+channel_name = channel_info["channel"]["name"]
+
+@app.command("/tokumei")
+def tokumei_post_handler(ack, respond, command):
+    tokumei.post_message(ack, respond, command, client, channel_info["channel"])
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
